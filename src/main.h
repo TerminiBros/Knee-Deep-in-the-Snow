a472216097ff46921aa702cee0bca15094ebbbdc
Paperdomo101 <38697390+Paperdomo101@users.noreply.github.com>
2022-12-10 22:23:33 +0800

    font

--- src/main.h
+++ src/main.h
@@ -175,6 +175,7 @@ void LoadAssets(void) { // Loads textures, shaders, audio, fonts, etc.
 
     sfxPause = LoadSound("assets/audio/pause.ogg");                                 // Load a sound
     fntLilLabels = LoadFontEx("assets/fonts/lil_labels.ttf", 7, 0, 0);              // Load a font
+    fntReadswell = LoadFontEx("assets/fonts/readswell.ttf", 13, 0, 0);              // Load a font
     SetTextureFilter(GetFontDefault().texture, TEXTURE_FILTER_POINT);               // Ensure default font is pixelated
 }
 
